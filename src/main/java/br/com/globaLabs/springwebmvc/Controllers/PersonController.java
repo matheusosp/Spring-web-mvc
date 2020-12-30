59e15e92da35a3f86cd15417097b096c774c28f2
matheusosp <matheusosp@users.noreply.github.com>
2020-12-30 15:57:03 -0300

    new Person impleted

--- src/main/java/br/com/globaLabs/springwebmvc/Controllers/PersonController.java
+++ src/main/java/br/com/globaLabs/springwebmvc/Controllers/PersonController.java
@@ -1,14 +1,31 @@
 package br.com.globaLabs.springwebmvc.Controllers;
 
+import br.com.globaLabs.springwebmvc.Model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class PersonController {
 
     @GetMapping("/person")
-    public String person(){
-        return "Person";
+    public ModelAndView person(){
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("Person");
+        final Person Luke = new Person("Luke", "Skywalker");
+        modelAndView.addObject("allPerson", List.of(Luke));
+        return modelAndView;
     }
 
+    @GetMapping("/new-Person")
+    public ModelAndView newPerson(){
+
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("New Person");
+
+        modelAndView.addObject("person", new Person());
+        return modelAndView;
+    }
 }
